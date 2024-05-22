e41233a4f36125b316c799dd5212fec32c8c9531
Dipto1971 <dipto.haq771@gmail.com>
2024-05-22 18:44:43 +0600

    Initial code base

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
+import React from 'react';
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World! This is my first React App. I am learning React and it is fun.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );
 }
