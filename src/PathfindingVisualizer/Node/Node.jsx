0e6cc69b53d17fb2cc8c3320eb25843c541cbd6e
Dipto1971 <dipto.haq771@gmail.com>
2024-05-25 18:42:55 +0600

    Initiated weights for each graph.

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -1,34 +1,37 @@
-import React, {Component} from 'react';
-
+// Node/Node.js
+import React from 'react';
 import './Node.css';
 
-export default class Node extends Component {
-  render() {
-    const {
-      col,
-      isFinish,
-      isStart,
-      isWall,
-      onMouseDown,
-      onMouseEnter,
-      onMouseUp,
-      row,
-    } = this.props;
-    const extraClassName = isFinish
-      ? 'node-finish'
-      : isStart
-      ? 'node-start'
-      : isWall
-      ? 'node-wall'
-      : '';
+const Node = ({ col, isFinish, isStart, isWall, weight, onMouseDown, onMouseEnter, onMouseUp, row }) => {
+  const extraClassName = isFinish
+    ? 'node-finish'
+    : isStart
+    ? 'node-start'
+    : isWall
+    ? 'node-wall'
+    : '';
 
-    return (
-      <div
-        id={`node-${row}-${col}`}
-        className={`node ${extraClassName}`}
-        onMouseDown={() => onMouseDown(row, col)}
-        onMouseEnter={() => onMouseEnter(row, col)}
-        onMouseUp={() => onMouseUp()}></div>
-    );
-  }
-}
+  return (
+    <div
+      id={`node-${row}-${col}`}
+      className={`node ${extraClassName}`}
+      onMouseDown={(e) => {
+        e.preventDefault();
+        onMouseDown(row, col);
+      }}
+      onMouseEnter={(e) => {
+        e.preventDefault();
+        onMouseEnter(row, col);
+      }}
+      onMouseUp={(e) => {
+        e.preventDefault();
+        onMouseUp();
+      }}
+    >
+      {weight}
+    </div>
+  );
+};
+
+export default Node;
+ 
